f04bdd91cc3f678ce2503a7dca552adf86a90de2
fckppag <87424710+fckppag@users.noreply.github.com>
2022-04-27 01:32:31 +0300

    upd

--- api/serializers.py
+++ api/serializers.py
@@ -8,7 +8,7 @@ class PostSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Post
-        fields = ['id', 'title', 'body', 'owner', 'public']
+        fields = ['id', 'title', 'body', 'owner']
 
 
 class UserSerializer(serializers.ModelSerializer):
