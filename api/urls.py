0327e89b5305345796c399a46c9a2c17f95e72e9
fckppag <87424710+fckppag@users.noreply.github.com>
2022-04-26 16:36:30 +0300

    task

---
+++ api/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from rest_framework.urlpatterns import format_suffix_patterns
+from . import views
+
+urlpatterns = [
+    path('users/', views.UserList.as_view()),
+    path('users/<int:pk>/', views.UserDetail.as_view()),
+    path('posts/', views.PostList.as_view()),
+    path('posts/<int:pk>/', views.PostDetail.as_view()),
+    path('api-auth/', include('rest_framework.urls')),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
---
+++ api/views.py
@@ -0,0 +1,32 @@
+from rest_framework import generics, permissions
+from . import serializers
+from django.contrib.auth.models import User
+
+from .models import Post
+from .permissions import IsOwnerOrReadOnly, IsWriterOrReadOnly
+from .serializers import PostSerializer
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = serializers.UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = serializers.UserSerializer
+
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,IsWriterOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly, IsWriterOrReadOnly]
